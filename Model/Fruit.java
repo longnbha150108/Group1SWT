71d83157a26ba8c228296801bf0d5bd2076403c7
longnbha150108 <107103970+longnbha150108@users.noreply.github.com>
2022-07-17 16:15:36 +0700

    Add files via upload

---
+++ Model/Fruit.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author notur
+ */
+public class Fruit {
+    String name;
+
+    public Fruit() {
+    }
+
+    public Fruit(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Fruit{" + "name=" + name + '}';
+    }
+    
+    
+}
